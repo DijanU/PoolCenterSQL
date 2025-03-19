05b12266ee4004b3264a238c278cec2f8a76f601
DijanU <pad23663@uvg.edu.gt>
2025-03-19 15:17:12 -0600

    balance fixes

--- ddl.sql
+++ ddl.sql
@@ -31,14 +31,20 @@ CREATE TABLE usuarios_roles (
     FOREIGN KEY usuario_id REFERENCES usuarios.id
 );
 
-
 CREATE TABLE materiales (
     id SERIAL PRIMARY KEY,
-    nombre VARCHAR(255) NOT NULL,
-    cantidad INT NOT NULL CHECK (cantidad >= 0),
-    proveedor VARCHAR(255) NOT NULL
+    nombre VARCHAR(255) NOT NULL
 );
 
+CREATE TABLE movimiento_materiales (
+    id SERIAL PRIMARY KEY,
+    material_id integer NOT NULL,
+    tipo VARCHAR(255) NOT NULL,
+    cantidad integer NOT NULL,
+    proveedor VARCHAR(255) NOT,
+    fecha date NOT NULL,
+    FOREIGN KEY material_id REFERENCES materiales.id
+);
 
 CREATE TABLE proyectos (
     id SERIAL PRIMARY KEY,
@@ -47,15 +53,29 @@ CREATE TABLE proyectos (
     presupuesto DECIMAL(10,2) NOT NULL CHECK (presupuesto >= 0),
     cliente VARCHAR(255) NOT NULL,
     fecha_inicio DATE NOT NULL,
-    fecha_fin DATE NOT NULL
+    fecha_fin DATE 
 );
 
 
-CREATE TABLE asignacion_material (
+CREATE TABLE material_proyecto (
     id SERIAL PRIMARY KEY,
     material_id INT NOT NULL,
     proyecto_id INT NOT NULL,
-    cantidad INT NOT NULL CHECK (cantidad > 0),
+    ofrecido INT NOT NULL CHECK (ofrecido > 0),
+    comprado INT NOT NULL CHECK (comprado > 0),
+    obra INT NOT NULL CHECK (obra > 0),
+    bodega INT NOT NULL CHECK (bodega > 0),
+    FOREIGN KEY material_id REFERENCES materiales.id,
+    FOREIGN KEY proyecto_id REFERENCES proyectos.id
+);
+
+CREATE TABLE bodega_proyecto (
+    id SERIAL PRIMARY KEY,
+    material_id integer NOT NULL,
+    tipo varchar(255) NOT NULL,
+    cantidad integer NOT NULL,
+    proyecto_id integer NOT NULL,
+    fecha date NOT NULL,
     FOREIGN KEY material_id REFERENCES materiales.id,
     FOREIGN KEY proyecto_id REFERENCES proyectos.id
 );
@@ -64,6 +84,6 @@ CREATE TABLE reportes (
     id SERIAL PRIMARY KEY,
     proyecto_id INT NOT NULL,
     fecha DATE NOT NULL,
-    contenido TEXT NOT NULL,
+    contenido TEXT,
     FOREIGN KEY proyecto_id REFERENCES proyectos.id
 );
